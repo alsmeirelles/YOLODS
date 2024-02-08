633f90ce8f6b86960ca1d66d6cab14ce043bd0c6
unknown <alsmeirelles@gmail.com>
2024-02-08 18:34:41 -0300

    Added background generation, annotation check and refined classe merge

--- mergeDS.py
+++ mergeDS.py
@@ -25,6 +25,7 @@ def Merge(config):
     with open(os.path.join(config.target,ytarget[0]),'r') as f:
         target_service = yaml.safe_load(f)
 
+    bgcount = 0
     for d in ['test','train','valid']:
         if config.verbose:
             print("***** STARTING ANALYSIS ({}) *****".format(d))
@@ -42,6 +43,10 @@ def Merge(config):
             for line in annotations:
                 sl = line.split(' ')
                 cl = target_service["names"][int(sl[0])].lower()
+                if len(sl) > 5: #Discard odd annotations
+                    print(f"Annotation has wrong format ({image}):\n - {sl}")
+                    continue
+
                 if cl in config.classes:
                     if dest_annotations is None:
                         dest_annotations = open(os.path.join(config.base,d,"labels",os.path.basename(timages[image])),"w")
@@ -50,30 +55,42 @@ def Merge(config):
                         print("Converting class {} from target to class {} in base set".format(cl,config.classes[cl]))
                     dest_cl = base_service["names"].index(config.classes[cl])
                     sl[0] = str(dest_cl) #Assign class index from base set to the synonym
+
                     dest_annotations.write(' '.join(sl))
                     shutil.copy(os.path.join(config.target,d,"images",image),os.path.join(config.base,d,"images")) #copy image to base set
+                elif config.bground:
+                    if dest_annotations is None:
+                        dest_annotations = open(os.path.join(config.base,d,"labels",os.path.basename(timages[image])),"w")
+                    if config.verbose:
+                        print("Adding original class {} from target as background in base set".format(cl))
+                    shutil.copy(os.path.join(config.target,d,"images",image),os.path.join(config.base,d,"images")) #copy image to base set
+                    bgcount += 1
+
             if not dest_annotations is None:
                 dest_annotations.close()
-
+    if config.bground:
+        print(f"Total of background added: {bgcount}")
 
 if __name__ == "__main__":
 
     #Parse input parameters
     arg_groups = []
-    synms = {"guns":"Gun","pistol":"Gun","handgun":"Gun","rifle":"Gun"}
+    synms = {"gun":"Gun","guns":"Gun","pistol":"Gun","handgun":"Gun","rifle":"Gun","knife":"Knife","steel arms":"Knife"}
 
     parser = argparse.ArgumentParser(description='Merge YoLo format datasets into one\
         dataset.')
     parser.add_argument('-p', dest='perc', type=float,
         help='Merge X% of the target dataset into the base set (Default: 100%)', default=1.0,required=False)
     parser.add_argument('-bs', dest='base', type=str,
-        help='Base dataset path, to which target will be merged into', default='',required=True)
+        help='Base dataset path', default='',required=True)
     parser.add_argument('-target', dest='target', type=str, default='',
         help='Target dataset path.',required=True)
     parser.add_argument('-classes', dest='classes', type=json.loads, default=synms,
         help='Class names equivalency (should be a dictionary formatted as string, targets are lower case, base is as writen in base set YAML).',required=False)
     parser.add_argument('-v', action='store_true', dest='verbose',
         help='Output merging messages.',default=False)
+    parser.add_argument('-bg', action='store_true', dest='bground',
+        help='Add other classes as background.',default=False)
 
     config, unparsed = parser.parse_known_args()
 
