1f654de1f28fe5542c3345d1aca6512dc301f31a
unknown <alsmeirelles@gmail.com>
2024-04-16 15:37:50 -0300

    Added frame extraction basics. Get X frames/second

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8
 #ALSMEIRELLES
-
+import concurrent.futures
 import os
 import sys
-import numpy as np
 import cv2
 import argparse
+import random
 
-#Yolo
-from ultralytics import YOLO as yolo
 
-def run_frame_extraction(config):
+# noinspection PyTypeChecker
+def run_frame_extraction(config: argparse.Namespace):
     """
     Searches for video files in data directory.
-    For each video found, inspect every X video frames in search for cars
-    If a car is found, extract the next Y frames to destination directory
+    For each video found, inspect every config.ns video frames every second
+    @type config: argparse namespace object
     """
     extracted_frames = 0
     analyzed_videos = 0
@@ -23,110 +22,99 @@ def run_frame_extraction(config):
     #Searches for known video files in source directory
     extensions = ['mp4','avi']
 
+    input_files = None
     #Check data source
-    if config.data is None:
-        print("No data source defined. Define the -data option.")
+    if os.path.isfile(config.fdata):
+        input_files = [config.fdata]
+    elif os.path.isdir(config.vdata):
+        input_files = list(filter(lambda x: x.split('.')[1] in extensions, os.listdir(config.vdata)))
+    else:
+        print("Video data not found.")
         sys.exit(1)
 
-    #Check what is the last image # in destination dir
-    dst_imgs = os.listdir(config.dst_dir)
-    dst_imgs = list(filter(lambda x: x.endswith('.jpg'),dst_imgs))
-    if len(dst_imgs) > 0:
-        dst_imgs = list(map(lambda x: int(x.split('.')[0]),dst_imgs))
-        dst_imgs.sort()
-        last_img = dst_imgs[-1]
+    if config.cpu > 1 and len(input_files) > 1:
+        from concurrent.futures import ThreadPoolExecutor
+        from tqdm import tqdm
+
+        bar = tqdm(desc="Processing video files...", total=len(input_files), position=0)
+        with ThreadPoolExecutor(max_workers=config.cpu) as executor:
+            videos = {executor.submit(_run_extractor,
+                                      os.path.join(config.vdata, f),
+                                      config.out,
+                                      config.ns,
+                                      config.verbose,
+                                      ): f for f in input_files}
+        for task in concurrent.futures.as_completed(videos):
+            bar.update(1)
+            extracted_frames += task.result()
+            analyzed_videos += 1
     else:
-        last_img = 0
+        for f in input_files:
+            if config.verbose:
+                print(f"Extracting from {f}...")
 
-    if os.path.isfile(config.mclasses):
-        classes_path = config.mclasses
-    else:
-        classes_path = os.path.join('data','classes',config.classes)
-
-    with open(classes_path) as f:
-        class_names = f.readlines()
-    class_names = {class_names[c].strip():c for c in range(len(class_names))}
-
-    if config.find_class in class_names:
-        class_n = class_names[config.find_class]
-    else:
-        print("Specified class is not detectable by YOLO: {}".format(config.find_class))
-        return None
-
-    pred_args = {
-        "model_path": os.path.join(config.model_path,'YOLO-trained_weights_final.h5'),
-        "anchors_path": config.anchors,
-        "classes_path": classes_path,
-        "gpu_num" : config.gpu_count}
-
-    predictor = YOLO(**pred_args)
-
-    input_files = os.listdir(config.data)
-    for f in input_files:
-        if config.info:
-            print("Extracting from {}...".format(f))
-        if os.path.isfile(os.path.join(config.data,f)) and f.split('.')[1] in extensions:
-            new_imgs = _run_extractor(predictor,os.path.join(config.data,f),config.dst_dir,
-                                          config.frame_inter,config.frame_count,last_img,class_n,
-                                          config.threshold)
+            new_imgs = _run_extractor(os.path.join(config.vdata, f), config.out, config.ns,config.verbose)
             extracted_frames += new_imgs
-            last_img += new_imgs
             analyzed_videos += 1
 
-    print("Extracted {} frames from {} videos".format(extracted_frames,analyzed_videos))
-    print("Total available images: {}".format(last_img))
+    print("Extracted {} frames from {} videos".format(extracted_frames, analyzed_videos))
 
-
-def _run_extractor(predictor,video_path,dst_dir,frame_interval,frame_count,last_img,class_n,ts):
+def _run_extractor(video_path:str, dst_dir:str, nfps:int, verbosity:int = 0):
     """
     Runs extraction
     """
+    random.seed()
     vid = cv2.VideoCapture(video_path)
     if not vid.isOpened():
         print("Video could not be opened: {}".format(video_path))
         return 0
 
+    video_name = os.path.basename(video_path)[:-4]
     vid_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    if verbosity >= 1:
+        print(f"Total frames in video: {vid_frames}; {fps} FPS")
+
     fcount = 0
     extracted = 0
     while fcount < vid_frames:
-        vid.set(cv2.CAP_PROP_POS_FRAMES, fcount)
-        ret, frame = vid.read()
-        if ret and predictor.detect_class(frame,class_n,ts):
-            cv2.imwrite(os.path.join(dst_dir,'{}.jpg'.format(last_img+extracted)),frame)
-            fcount += 1
-            extracted += 1
-            for j in range(frame_count):
-                if j >= vid_frames:
-                    break
-                vid.set(cv2.CAP_PROP_POS_FRAMES, fcount)
-                ret, frame = vid.read()
-                if ret:
-                    cv2.imwrite(os.path.join(dst_dir,'{}.jpg'.format(last_img+extracted)),frame)
-                    extracted += 1
-                else:
-                    print("Error reading frame {} from video {}".format(fcount,video_path))
-                fcount += 1
-            fcount += frame_interval
-        else:
-            fcount += frame_interval
-            print("Steping to frame {}".format(fcount))
+        fi = random.sample(range(fcount, min(fcount+fps, vid_frames)), k=nfps)
+
+        for j in fi:
+            vid.set(cv2.CAP_PROP_POS_FRAMES, j)
+            ret, frame = vid.read()
+            if ret:
+                cv2.imwrite(os.path.join(dst_dir, '{}-{}.jpg'.format(video_name, j)), frame)
+                extracted += 1
+            else:
+                print("Error reading frame {} from video {}".format(j, video_path))
+        fcount += fps
 
     vid.release()
     return extracted
 
 if __name__ == "__main__":
 
-    #Parse input parameters
+    # Parse input parameters
     arg_groups = []
-    parser = argparse.ArgumentParser(description='Extract frames to be used to train an YOLO model .')
+    parser = argparse.ArgumentParser(description='Extract frames from video files.')
 
     parser.add_argument('-vdata', dest='vdata', type=str, default='',
-        help='Location of video files.',required=True)
+        help='Path to folder containing video files.',required=False)
     parser.add_argument('-fdata', dest='fdata', type=str, default='',
-        help='Location of annotation files.',required=True)
-
-
+        help='Path to a specific video file.',required=False)
+    parser.add_argument('-out', dest='out', type=str, default='',
+        help='Save frames here.',required=True)
+    parser.add_argument('-ns',dest='ns', type=int, default=3,
+        help='Extract this many frames per second (Set to zero for all).')
+    parser.add_argument('-cpu', dest='cpu', type=int, default=1,
+        help='Multiprocess extraction. Define the number of processes (Default=1).')
+    parser.add_argument('-v', action='count', default=0, dest='verbose',
+        help='Amount of verbosity (more \'v\'s means more verbose).')
     config, unparsed = parser.parse_known_args()
+
+    if not config.vdata and not config.fdata:
+        print('You should define a path to a dir (vdata) or file (fdata)')
+        sys.exit(1)
 
     run_frame_extraction(config)
